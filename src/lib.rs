42a909bbcc1381c8d1f3d24904fc37165c8e675e
Philipp Gawlik <philipp.gawlik@googlemail.com>
2019-02-05 08:27:26 +0100

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -75,10 +75,11 @@ fn get_probability(nominator: &i32, denominator: &i32) -> f32 {
 }
 
 pub fn model(config: &Config) -> Result<(), Box<dyn Error>> {
-    let mut counts: HashMap<(char, char, char), i32> = HashMap::new();
     let content = fs::read_to_string(&config.filename)
         .expect(&format!("Failed to read from {}", &config.filename))
         .replace("\n", "");
+    // count threegrams
+    let mut counts: HashMap<(char, char, char), i32> = HashMap::new();
     let _ = get_threegram_iter(&content).map(|ngram| {
         let count = counts.entry(ngram).or_insert(0);
         *count += 1;
